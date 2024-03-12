d651227c5607c5a17fe176e5139719659b4bd827
Alvaro <alvarorodriguezam22@gmail.com>
2024-03-12 18:55:18 +0100

    Actualización contenido

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert('Bienvenido a En busca del Grammy. ¡Ayuda a Anuel a encontrar el Grammy!');
+    // Solicitamos la edad al usuario mediante un cuadro de diálogo
+    var age = prompt('Por favor, ingresa tu edad:');
+    
+    // Verificamos si la edad es válida y si el usuario puede ingresar a la web
+    if (age >= 18) {
+        // Si la edad es mayor o igual a 18, le damos la bienvenida 
+        alert('¡Bienvenido a la web oficial de "En busca del Grammy"! ¿Serás capaz de ayudar a Anuel con sus Grammys?');
+    } else {
+        // Si la edad es menor de 18, enviamos al usuario a una web según su rango de edad
+        if (age >= 0 && age <= 3) {
+            window.location.href = 'https://www.dodot.es/';
+        } else if (age >= 4 && age <= 6) {
+            window.location.href = 'https://www.pocoyo.com/';
+        } else if (age >= 7 && age <= 12) {
+            window.location.href = 'https://www.wizardingworld.com/';
+        } else if (age >= 13 && age <= 17) {
+            window.location.href = 'https://www.lego.com/';
+        } else {
+            // Mensaje de error
+            alert('La edad ingresada no es válida. Por favor, ingresa una edad válida.');
+        }
+    }
 });
